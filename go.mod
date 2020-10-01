8a1446bacca4ab32395358f1776abca9ab81ae6a
jasondavindev <jasondavin.dev@gmail.com>
2020-10-01 11:10:59 -0300

    start project

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/jasondavindev/hacktoberfest-2020
+
+go 1.15
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/joho/godotenv v1.3.0
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	environment := os.Getenv("GOENV")
+	directoryWatch := os.Getenv("DIRECTORY")
+	fmt.Println(environment)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(directoryWatch)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
